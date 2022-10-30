cce6043d7b54ff4d92a350f1d00c9983930e6151
1ekk <akatsukiserver1@gmail.com>
2022-10-31 01:03:22 +0300

    :(

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    GOOGLE_URL: process.env.GOOGLE_URL,
+  }
 }
 
 module.exports = nextConfig
--- slices/apiSlice.js
+++ slices/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://script.google.com/macros/s/' + process.env.GOOGLE_URL,
+        baseUrl: 'https://script.google.com/macros/s/AKfycbx4MPmt18Hp-1Lz7UdfQD0uJ-Ih1B4HHSWFG0a6qdlnOrnnFzirO7u8NjTg6jFlXRAB/exec',
         mode: 'cors'
     }),
     endpoints: builder => ({
