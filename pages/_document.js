b123a2ccee326fdfe1f127640a5cc761b45f9460
1ekk <akatsukiserver1@gmail.com>
2022-10-31 02:34:31 +0300

    HTML FIX

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
+    <Html data-theme="dark">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@500&family=Teko:wght@500&display=swap" rel="stylesheet"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
